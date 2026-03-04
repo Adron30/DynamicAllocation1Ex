eacca1032ca8e177b8621445d3d5455530563818
Marc Moreno <65243413+Adron30@users.noreply.github.com>
2026-03-04 21:13:06 +0100

    Comment to understand things

--- main.c
+++ main.c
@@ -19,6 +19,16 @@ int main() {
             printf("Stored value %d at %p\n", i, &memoryaddres[i]);
         }
 
+        /*
+        //Using memory as list what really does is getting the address
+         from malloc and using i*<dataype> (the datatype is defined by the type of pointer at the begining) to asign the value to the corresponding offset
+         memoryadress[0 * 4] = Add '0' to my initial memory offset -> ...9e0
+         memoryadress[1 * 4] = Add '4' to my initial memory offset -> ...9e4
+         memoryadress[2 * 4] = Add '8' to my initial memory offset -> ...9e8
+         memoryadress[3 * 4] = Add '12' to my initial memory offset -> ...9ec
+         ..
+        */
+
         free(memoryaddres);
         memoryaddres = NULL;
     }
